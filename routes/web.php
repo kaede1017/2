b477772a9359f0912a8aa2d99d2df14acfcd9be7
kaede1017 <maple10178327@gmail.com>
2023-03-01 15:05:20 +0900

    適切なコミットコメント

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
+use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
 
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/posts', [PostController::class, 'index']);  
+

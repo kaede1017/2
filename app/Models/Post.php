322349f58531547ad400d230aa57526a74445b9c
kaede1017 <maple10178327@gmail.com>
2023-03-12 17:21:36 +0900

    適切なコミットコメント

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,10 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Post extends Model 
+
+
+
 
 {
     use HasFactory;
+    
+    protected $fillable = [
+    'title',
+    'body',
+        ];
     
     public function getPaginateByLimit(int $limit_count = 5)
     { 
